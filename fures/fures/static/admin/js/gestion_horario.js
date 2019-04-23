918b84ab541c31084377649cfb44666dff1d58e9
Ledinalvarado <ledinalvarado18@gmail.com>
2019-04-23 12:07:32 -0600

    agregando la parte administradora  *Sucursal

---
+++ fures/fures/static/admin/js/gestion_horario.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+        var doc = $(document);
+        $('a.add').die('click').live('click', function (e) {
+            e.preventDefault();
+            var content = $('#type-container .type-row'),
+                element = null;
+            for (var i = 0; i < 1; i++) {
+                element = content.clone();
+                var type_div = 'teams_' + $.now();
+                element.attr('id', type_div);
+                element.find('.remove').attr('targetDiv', type_div);
+                element.appendTo('#type_contenedor');
+
+            }
+        });
+
+        $(".remove").die('click').live('click', function (e) {
+            var didConfirm = confirm("Está seguro de que quiere borrar ésta imagen?");
+            if (didConfirm == true) {
+                var id = $(this).attr('data-id');
+                var targetDiv = $(this).attr('targetDiv');
+                //if (id == 0) {
+                //var trID = jQuery(this).parents("tr").attr('id');
+                $('#' + targetDiv).remove();
+                // }
+                return true;
+            } else {
+                return false;
+            }
+        });
+
+    });
